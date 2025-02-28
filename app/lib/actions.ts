275e67e0e2622343c002d32501b4cb1d445386dc
David Ngassa <David.Ngassa@dayforce.com>
2025-02-28 22:36:12 +0400

    fix build

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,6 +61,7 @@ export async function createInvoice(prevState: State, formData: FormData){
       `;
     }
     catch(error){
+        console.log(error);
         // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Create Invoice.',
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function LoginForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
-  const [errorMessage, formAction, isPending] = useActionState(
+  const [errorMessage, formAction] = useActionState(
     authenticate,
     undefined,
   );
